458a88743c3b59d0088683a7538d6685b9e61c36
Kyle King <KyleKing@users.noreply.github.com>
2024-07-31 21:50:56 -0400

    release: v0.0.3

--- mdformat_obsidian/__init__.py
+++ mdformat_obsidian/__init__.py
@@ -1,5 +1,7 @@
 """An mdformat plugin for `obsidian`."""
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from .plugin import RENDERERS, update_mdit
+
+__all__ = ("RENDERERS", "__version__", "update_mdit")
